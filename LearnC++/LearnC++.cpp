b7901c5b38fdc91d65c38c3fab43e9f16944583c
//webnono// <webnono858@gmail.com>
2023-05-14 10:58:12 +0200

    Ajoutez des fichiers projet.

---
+++ LearnC++/LearnC++.cpp
@@ -0,0 +1,9 @@
+// ----------------------------------- LearnC++.cpp for LearnC++ ----------------------------------- //
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
